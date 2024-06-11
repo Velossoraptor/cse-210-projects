311b6ad7d0ec295e0c4fd25f36539dad759b73c1
Velossoraptor <taliamolsen@gmail.com>
2024-06-11 14:57:44 -0400

    Finished Prep 5 - Username and number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Net.NetworkInformation;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNum = PromptUserNumber();
+        int squared = SquareNumber(userNum);
+        DisplayResult(userName, squared);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your username?: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favourite number?: ");
+        int favNum = int.Parse(Console.ReadLine());
+        return favNum;
+    }
+    static int SquareNumber(int number)
+    {
+        int squared = number * number;
+        return squared;
+    }
+    static void DisplayResult(string userName, int squared)
+    {
+        Console.WriteLine($"{userName}, your number squared is {squared}.");
+
     }
 }
