03162a2c0dd01e2300b6fcbb8d6ba24685bc68a8
Velossoraptor <taliamolsen@gmail.com>
2024-06-11 14:44:29 -0400

    Finished Prep 4 - List of numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, when finished type 0.");
+        int userIn = 0;
+        int sum = 0;
+        float avg = 0;
+        int largest = 0;
+        int smallestPos = 9999999;
+        List<int> numbers = new List<int>();
+        do
+        {
+            userIn = int.Parse(Console.ReadLine());
+            if (userIn > largest)
+            {
+                largest = userIn;
+            }
+            if (userIn < smallestPos && userIn > 0)
+            {
+                smallestPos = userIn;
+            }
+            if (userIn != 0)
+            {
+                numbers.Add(userIn);
+                sum += userIn;
+            }
+        }
+        while (userIn != 0);
+        avg = (float)sum / (numbers.Count);
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallestPos}");
+        numbers.Sort();
+        Console.WriteLine("The sorted list is: ");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
