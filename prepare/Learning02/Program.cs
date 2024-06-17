20c8d19c929882c7ebf3a64e58e77d2f27c4ceb4
Velossoraptor <taliamolsen@gmail.com>
2024-06-17 11:50:34 -0400

    Job program completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Rona";
+        job1._jobTitle = "Paint Associate";
+        job1._startYear = 2024;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "Glenburnie Grocery";
+        job2._jobTitle = "Deli";
+        job2._startYear = 2024;
+        job2._endYear = 2024;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Talia Olsen";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+
+        resume1.Display();
     }
 }
